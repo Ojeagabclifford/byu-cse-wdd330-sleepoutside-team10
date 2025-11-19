42a2408b19adb59bcbb180c9052041a2cd286fa6
ojeagaclifford <cli24003@byui.edu>
2025-11-19 21:18:09 +0100

    clifford did the menu

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,9 @@
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
-import { getParam, loadHeaderFooter } from './utils.mjs';
 
-loadHeaderFooter();
+const dataSource = new ProductData('tents');
+const productID = getParam('products');
 
-const productId = getParam('product');
-const dataSource = new ProductData();
-const product = new ProductDetails(productId, dataSource);
+const product = new ProductDetails(productID, dataSource);
 product.init();
